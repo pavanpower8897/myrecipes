bf154a27b5b5ebcfd1691c8ebb06a784c6ab481f
pavanpower8897 <pavanpower8897@gmail.com>
2020-05-12 11:47:54 +0530

    Add new recipe with tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,30 @@ class RecipesController < ApplicationController
 	def index
 		@recipes = Recipe.all
 	end
+
+	def show
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def new
+		@recipe = Recipe.new
+	end
+    def create
+    	@chef = Chef.first
+    	@recipe = @chef.recipes.build(recipe_params)
+        if @recipe.save
+        	flash[:notice] = "Recipe is succesfully created"
+        	redirect_to recipe_path(@recipe)
+        else
+        	render 'new'
+        end
+    	
+    end
+
+
+    private
+
+     def recipe_params
+     	params.require(:recipe).permit(:name,:description)
+     end
 end
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -17,7 +17,45 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
   	get recipes_path
   	assert_template 'recipes/index'
-  	assert_match @rec1.name, response.body
-  	assert_match @rec2.name, response.body
+    assert_select 'a[href=?]', recipe_path(@rec1), text: @rec1.name
+    assert_select 'a[href=?]', recipe_path(@rec2), text: @rec2.name
   end
+
+  test "should give recipes show page" do
+    get recipe_path(@rec1)
+    assert_response :success
+  end
+
+  test "should show recipe details" do
+    get recipe_path(@rec1)
+    assert_template 'recipes/show'
+    assert_match @rec1.name.capitalize, response.body
+    assert_match @rec1.description, response.body
+    assert_match @chef.chefname, response.body
+  end
+
+  test "create  new valid recipe" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = "Chicke salad"
+    description_of_recipe = "Add 2litres of water and 1kg chicken and boil them"
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: {recipe: {name: name_of_recipe, description: description_of_recipe}}
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+  end
+
+  test "reject invalid recipe submissions" do
+      get new_recipe_path
+      assert_template 'recipes/new'
+      assert_no_difference 'Recipe.count' do\
+        post recipes_path, params: { recipe: {name: " ", description: "  "}  }
+      end
+      assert_template 'recipes/new'
+        assert_select 'h2.panel-title'
+        assert_select 'div.panel-body'
+  end
+
 end
