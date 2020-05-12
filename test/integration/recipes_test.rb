78b8d89cb359b82c69809c430db1d98edb29ade2
pavanpower8897 <pavanpower8897@gmail.com>
2020-05-12 16:59:27 +0530

    refactor and cleanout layout

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -34,6 +34,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @chef.chefname, response.body
     assert_select "a[href=?]", edit_recipe_path(@rec1), text: "Edit this recipe"
     assert_select "a[href=?]", recipe_path(@rec1), text: "Delete this recipe"
+    assert_select "a[href=?]", recipes_path, text: "Return to recipes listing"
   end
 
   test "create  new valid recipe" do
